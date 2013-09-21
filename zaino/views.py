dd53a2887d1c8deb64b29410a127a0e891b53aec
Voidmaster <ramirotrvnt18@gmail.com>
2013-09-21 19:04:13 -0300

    Cuarto commit! Primera vista.

--- zaino/views.py
+++ zaino/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, render_to_response
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello World!")
